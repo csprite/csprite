e3d4cbb47baca6f2b8a0151b4ac40d8fe9eb7824
Aditya M <hi@pegv.in>
2022-04-24 20:09:12 +0530

    Disable Generation of .ini file

--- src/main.cpp
+++ src/main.cpp
@@ -269,7 +269,10 @@ int main(int argc, char **argv) {
 
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
+
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.IniFilename = NULL; // Disable Generation of .ini file
+
 	ImGui::StyleColorsDark();
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init("#version 330");
