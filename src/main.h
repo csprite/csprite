f137615c6fd10588ef953d16a8e8729b5ded27c3
Aditya M <hi@pegv.in>
2022-04-26 21:19:05 +0530

    Add Color Palette & Zoom Level in The UI
    Also Added Resizable Window, Now Window Can Be Resized And The Canvas Goes Back To Center

--- src/main.h
+++ src/main.h
@@ -3,12 +3,13 @@
 
 void framebuffer_size_callback(GLFWwindow *, int, int);
 void process_input(GLFWwindow *);
+void window_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow *, double, double);
 void mouse_button_callback(GLFWwindow *, int, int, int);
 void scroll_callback(GLFWwindow *, double, double);
 void key_callback(GLFWwindow *, int, int, int, int);
 void viewport_set();
-void adjust_zoom(int);
+void adjust_zoom(bool increase);
 int string_to_int(int *out, char *s);
 int color_equal(unsigned char *, unsigned char *);
 unsigned char * get_pixel(int x, int y);
