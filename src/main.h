8ede1bd0f0c91cf467281cb812f735cb58bf0afc
Aditya M <git@pegv.in>
2022-05-09 10:20:17 +0530

    Code Splitting

--- src/main.h
+++ src/main.h
@@ -3,8 +3,6 @@
 
 enum export_format_e { PNG, JPG };
 
-void openUrl(std::string url);
-void logGLFWErrors(int error, const char *description); // GLFW Error Callback
 void zoomAndLevelViewport(); // Zooms And Levels The Canvas.
 void framebuffer_size_callback(GLFWwindow* window, int, int);
 void process_input(GLFWwindow* window);
@@ -15,16 +13,10 @@ void scroll_callback(GLFWwindow* window, double, double);
 void key_callback(GLFWwindow* window, int, int, int, int);
 void viewport_set();
 void adjust_zoom(bool increase);
-int string_to_int(int *out, char *s);
-int color_equal(unsigned char *, unsigned char *);
 unsigned char * get_char_data(unsigned char *data, int x, int y);
 unsigned char * get_pixel(int x, int y);
 void fill(int x, int y, unsigned char *old_colour);
 void draw(int x, int y); // Draws on 1 x, y coordinate
 void drawInBetween(int st_x, int st_y, int end_x, int end_y);
-
-void LoadImageToCanvas();
-void SaveImageFromCanvas(std::string filepath);
-void WritePngFromCanvas(const char *filepath);
-void WriteJpgFromCanvas(const char *filepath);
 std::string FixFileExtension(std::string filepath);
+void SaveImageFromCanvas(std::string filepath);
---
+++ src/save.h
@@ -0,0 +1,50 @@
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "../lib/stb/stb_image_write.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "../lib/stb/stb_image.h"
+
+void LoadImageToCanvas(const char *filepath, int *canvas_dims, unsigned char **canvas_data) {
+	int imgWidth, imgHeight, c;
+	unsigned char *image_data = stbi_load(filepath, &imgWidth, &imgHeight, &c, 0);
+	if (image_data == NULL) {
+		printf("Unable to load image %s\n", filepath);
+		return;
+	}
+
+	canvas_dims[0] = imgWidth;
+	canvas_dims[1] = imgHeight;
+
+	if (*canvas_data != NULL) free(*canvas_data);
+
+	*canvas_data = (unsigned char *)malloc(canvas_dims[0] * canvas_dims[1] * 4 * sizeof(unsigned char));
+	memset(*canvas_data, 0, canvas_dims[0] * canvas_dims[1] * 4 * sizeof(unsigned char));
+	int j, k;
+	unsigned char *ptr;
+	unsigned char *iptr;
+	for (j = 0; j < imgHeight; j++) {
+		for (k = 0; k < imgWidth; k++) {
+			ptr = get_pixel(k, j);
+			iptr = get_char_data(image_data, k, j);
+			*(ptr+0) = *(iptr+0);
+			*(ptr+1) = *(iptr+1);
+			*(ptr+2) = *(iptr+2);
+			*(ptr+3) = *(iptr+3);
+		}
+	}
+	stbi_image_free(image_data);
+}
+
+void WritePngFromCanvas(const char *filepath, int *canvas_dims) {
+	unsigned char *data = (unsigned char *) malloc(canvas_dims[0] * canvas_dims[1] * 4 * sizeof(unsigned char));
+	glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+	stbi_write_png(filepath, canvas_dims[0], canvas_dims[1], 4, data, 0);
+	free(data);
+}
+
+void WriteJpgFromCanvas(const char *filepath, int *canvas_dims) {
+	unsigned char *data = (unsigned char *) malloc(canvas_dims[0] * canvas_dims[1] * 4 * sizeof(unsigned char));
+	glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+	stbi_write_jpg(filepath, canvas_dims[0], canvas_dims[1], 4, data, 100);
+	free(data);
+}
