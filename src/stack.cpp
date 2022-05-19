5c5b08108f90ee68e27e99b666b3d5859583f3dd
Aditya Mishra <75035219+DEVLOPRR@users.noreply.github.com>
2022-05-19 08:42:00 +0530

    A Simple Stack DS Implementation

---
+++ src/stack.cpp
@@ -0,0 +1,80 @@
+#include <cstdio>
+
+class Stack {
+private:
+	int top;
+	static constexpr unsigned int MAX_STATES = 5;
+	unsigned char *canvasStates[MAX_STATES] = { NULL };
+
+public:
+	Stack() {
+		top = -1;
+	}
+	~Stack() {
+		printf("Freeing Undo Redo Stack!\n");
+		for (int i = 0; i < (MAX_STATES - 1); i++) {
+			if (canvasStates[i] != NULL) {
+				free(canvasStates[i]);
+			}
+		}
+	}
+
+	bool isEmpty() {
+		return top == -1;
+	}
+
+	bool isFull() {
+		return top == (MAX_STATES - 1);
+	}
+
+	void push(unsigned char *CanvasData) {
+		if (isFull()) {
+			printf("Cannot Push Onto Stack, Stack is Full!\n");
+			return;
+		} else {
+			top++;
+			canvasStates[top] = CanvasData;
+		}
+	}
+
+	unsigned char * pop() {
+		if (isEmpty()) {
+			printf("Cannot Pop from the stack, Stack is empty!\n");
+			return NULL;
+		} else {
+			unsigned char *itemPopped = canvasStates[top];
+			canvasStates[top] = NULL;
+			top--;
+			return itemPopped;
+		}
+	}
+
+	int count() {
+		return (top + 1);
+	}
+
+	int size() {
+		return MAX_STATES;
+	}
+
+	unsigned char * peek(int position) {
+		if (isEmpty()) {
+			printf("Cannot Get Element from the stack, Stack is empty!\n");
+			return NULL;
+		} else {
+			return canvasStates[position];
+		}
+	}
+
+	void change(int position, unsigned char *CanvasData) {
+		canvasStates[position] = CanvasData;
+	}
+
+	void display() {
+		printf("----------------\n");
+		for (int i = (MAX_STATES - 1); i >= 0; i--) {
+			printf("> %p\n", (void*)canvasStates[i]);
+		}
+		printf("----------------\n");
+	}
+};
