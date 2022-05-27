707cedec3e19993f4a3f355af6f338362ef4c7d1
Aditya Mishra <75035219+DEVLOPRR@users.noreply.github.com>
2022-05-27 19:40:29 +0530

    Rename File

--- generateFont.sh
+++ generateFont.sh
@@ -3,5 +3,5 @@
 set -e
 
 rm -f include/FontMontserrat_Bold.h
-clang++ lib/binary_to_compressed_c.cpp -o bin2CompC
-./bin2CompC ./assets/fonts/Montserrat-Bold.ttf Montserrat_Bold > ./include/FontMontserrat_Bold.h
+clang++ lib/binary2header.cpp -o bin2header
+./bin2header ./assets/fonts/Montserrat-Bold.ttf Montserrat_Bold > ./include/FontMontserrat_Bold.h
