02a9b0d87d07423ddd791314c752a7d594bd7962
Aditya M <hi@pegv.in>
2022-08-13 20:04:55 +0530

    Add CheckerBoard Pattern

--- data/shaders/fragment.glsl
+++ data/shaders/fragment.glsl
@@ -1,5 +1,7 @@
 #version 130 // OpenGL v3.0
 
+// Todo: Bug Fix - Black Color is not shown
+
 /*
 	lowp     -  Low Precision     (Best for colors)
 	mediump  -  Medium Precision  (Best for Texture Co-ordinates)
@@ -18,6 +20,13 @@ uniform mediump vec2 offset;
 uniform lowp float alpha;
 
 void main() {
-	// Final Col = RGBA Output Col, Alpha * RGBA Color Of Texture At This Coordinate
-	frag_color = vec4(out_color, alpha) * texture(a_texture, out_tex_coords + offset);
+	vec3 chromaKeyColor = texture(a_texture, (out_tex_coords + offset).xy).xyz;
+	if ((chromaKeyColor.x <= 0.01) && (chromaKeyColor.y <= 0.01) && (chromaKeyColor.z <= 0.01)){
+		vec2 Pos = floor(gl_FragCoord.xy / 10.0); // 10.0 - Size of Squares
+		float PatternMask = mod(Pos.x + mod(Pos.y, 2.0), 2.0);
+		frag_color = PatternMask * vec4(1.0, 1.0, 1.0, 1.0);
+	} else {
+		// Final Col = RGBA Output Col, Alpha * RGBA Color Of Texture At This Coordinate
+		frag_color = vec4(out_color, alpha) * texture(a_texture, out_tex_coords + offset);
+	}
 }
