45bef1e58b05f2032d29f3935c9fa9df4254b4ed
Aditya M <hi@pegv.in>
2022-08-13 15:58:08 +0530

    Switch To OpenGL v3.0

--- data/shaders/fragment.glsl
+++ data/shaders/fragment.glsl
@@ -1,12 +1,21 @@
-#version 330 core
-out vec4 frag_color;
+#version 130 // OpenGL v3.0
 
-in vec3 out_color;
-in vec2 out_tex_coords;
+/*
+	lowp     -  Low Precision     (Best for colors)
+	mediump  -  Medium Precision  (Best for Texture Co-ordinates)
+	highp    -  High Precision    (Best for Vertex Positions)
+
+	Many systems don't support highp, so we're using mediump
+*/
+
+out lowp vec4 frag_color;
+
+in lowp vec3 out_color;
+in mediump vec2 out_tex_coords;
 
 uniform sampler2D a_texture;
-uniform vec2 offset;
-uniform float alpha;
+uniform mediump vec2 offset;
+uniform lowp float alpha;
 
 void main() {
 	// Final Col = RGBA Output Col, Alpha * RGBA Color Of Texture At This Coordinate
