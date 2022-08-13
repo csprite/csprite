f9a48c1e05c3edcef9e7f7f29a216b817c34d6d8
Aditya <75035219+pegvin@users.noreply.github.com>
2022-08-13 15:58:08 +0530

    Switch To OpenGL v3.0

--- data/shaders/vertex.glsl
+++ data/shaders/vertex.glsl
@@ -1,7 +1,7 @@
-#version 330 core
-layout (location = 0) in vec3 position;
-layout (location = 1) in vec3 color;
-layout (location = 2) in vec2 tex_coords;
+#version 130 // OpenGL v3.0
+in vec3 position;
+in vec3 color;
+in vec2 tex_coords;
 
 out vec3 out_color;
 out vec2 out_tex_coords;
