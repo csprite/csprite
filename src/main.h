b06d630c8b37861e2700212e993db13a64a39fc8
Aditya <75035219+pegvin@users.noreply.github.com>
2022-08-20 14:36:54 +0530

    Simple Prototype in SDL

--- src/main.h
+++ src/main.h
@@ -3,42 +3,13 @@
 
 #include <stdint.h>
 #include <string>
-#include "GLFW/glfw3.h"
 
 enum export_format_e { PNG, JPG };
 
-// Callbacks
-void ProcessInput(GLFWwindow* window);
-void ScrollCallback(GLFWwindow* window, double, double);
-void KeyCallback(GLFWwindow* window, int, int, int, int);
-void FrameBufferSizeCallback(GLFWwindow* window, int, int);
-void WindowSizeCallback(GLFWwindow* window, int width, int height);
-void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
-
-// Canvas Zoom
-void ViewportSet();
-void ZoomNLevelViewport(); // Zooms And Levels The Canvas.
-void AdjustZoom(bool increase);
-
 // Little Helpers
-unsigned char * GetPixel(int x, int y);
+Uint32* GetPixel(int x, int y, Uint32* data = NULL);
 
-// Canvas Operations
-void fill(int x, int y, unsigned char *old_colour);
 void draw(int x, int y); // Draws on 1 x, y coordinate
-void drawInBetween(int st_x, int st_y, int end_x, int end_y); // Draws Between The Given 2 x, y coordinates
-void drawLine(int lStartX, int lStartY, int lEndX, int lEndY);
-void drawRect(int x0, int y0, int x1, int y1);
-
-// File IO
-std::string FixFileExtension(std::string filepath);
-void SaveImageFromCanvas(std::string filepath);
-
-// Undo/Redo Related
-int Undo();
-int Redo();
-void SaveState();
-void FreeHistory();
 
 // Versions & Stuff
 
