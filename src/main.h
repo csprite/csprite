0845d418179ba42836eb9e06df1e75dc7f7228d0
Aditya M <hi@pegv.in>
2022-08-20 17:33:00 +0530

    Fix Line Gaps On Low Frame Rates

--- src/main.h
+++ src/main.h
@@ -11,7 +11,9 @@ Uint32* GetPixel(int x, int y, Uint32* data = NULL);
 
 void ProcessEvents();
 void UpdateCanvasRect();
+
 void draw(int x, int y); // Draws on 1 x, y coordinate
+void drawInBetween(int st_x, int st_y, int end_x, int end_y);
 
 // Versions & Stuff
 
