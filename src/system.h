b0da3d9faf6cd7b9973f2bc82b857d3633af10ee
Aditya M <git@pegv.in>
2022-08-30 15:00:50 +0530

    Add Path Separator

--- src/system.h
+++ src/system.h
@@ -1,8 +1,16 @@
 #ifndef SYSTEM_H
 #define SYSTEM_H
 
+#include <stdio.h>
+
 int sys_list_dir(const char *dir, int (*callback)(const char *dir, const char *name, void* data), void* data);
 int sys_make_dir(const char *path);
 long int fsize(FILE* f);
+
+#if defined(_WIN32) || defined(WIN32)
+	#define SYS_PATH_SEP '\\'
+#else
+	#define SYS_PATH_SEP '/'
+#endif
 
 #endif // SYSTEM_H
