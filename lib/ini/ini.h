3f6d80d8b4142ce1ccc7ffb91d6b63303ed98c13
Aditya <75035219+pegvin@users.noreply.github.com>
2022-09-03 01:27:48 +0530

    Add Loading Ini From String

--- lib/ini/ini.h
+++ lib/ini/ini.h
@@ -17,6 +17,7 @@ extern "C" {
 typedef struct ini_t ini_t;
 
 ini_t*      ini_load(const char *filename);
+ini_t*      ini_load_txt(const char *iniTxt);
 void        ini_free(ini_t *ini);
 const char* ini_get(ini_t *ini, const char *section, const char *key);
 int         ini_sget(ini_t *ini, const char *section, const char *key, const char *scanfmt, void *dst);
