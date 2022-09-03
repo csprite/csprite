3ed671af13688cb42c7fb9a500e42381991e9c0c
Aditya <75035219+pegvin@users.noreply.github.com>
2022-09-03 15:25:41 +0530

    Implement Full Theming System

--- src/theme.c
+++ src/theme.c
@@ -1,9 +1,17 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "theme.h"
 #include "ini/ini.h"
 #include "system.h"
 #include "macros.h"
+#include "cconfig.h"
+#include "log/log.h"
+#include "assets.h"
+#include "system.h"
+
+static int OnSysDirList(const char *dir, const char *fname, void* data);
+static int OnAssetMgrList(int i, const char *fname);
 
 static Uint32 str2rgbaint(const char* str) {
 	if (str == NULL) {
@@ -20,6 +28,32 @@ void _PrintColor_T(Uint32 col) {
 	printf("Color: %08x\n", col);
 }
 
+int FreeTheme(theme_t* theme) {
+	if (theme == NULL) return -1;
+
+	free(theme);
+	return 0;
+};
+
+int FreeThemeArr(theme_arr_t* tArr) {
+	if (tArr == NULL) {
+		log_error("Theme Array Pointer is NULL!");
+		return -1;
+	}
+
+	if (tArr->entries != NULL) {
+		for (int i = 0; i < tArr->numOfEntries; ++i) {
+			FreeTheme(tArr->entries[i]);
+			tArr->entries[i] = NULL;
+		}
+		free(tArr->entries);
+		tArr->entries = NULL;
+	}
+
+	free(tArr);
+	return 0;
+};
+
 theme_t* LoadTheme(const char* themeIni) {
 	if (themeIni == NULL) {
 		return NULL;
@@ -27,6 +61,12 @@ theme_t* LoadTheme(const char* themeIni) {
 
 	theme_t* t = malloc(sizeof(theme_t));
 	ini_t *config = ini_load_txt(themeIni);
+
+	const char* name = ini_get(config, "theme", "name");
+	if (name != NULL)
+		strncpy(t->name, name, 512);
+	else
+		strncpy(t->name, "theme", 512);
 
 	t->PopupBG = str2rgbaint(ini_get(config, "theme", "PopupBG"));
 	t->WindowBG = str2rgbaint(ini_get(config, "theme", "WindowBG"));
@@ -48,4 +88,84 @@ theme_t* LoadTheme(const char* themeIni) {
 	ini_free(config);
 	config = NULL;
 	return t;
+}
+
+
+theme_arr_t* ThemeLoadAll() {
+	char* themesDirPath = SysGetThemesDir();
+	char dir[CC_PATH_SIZE_MAX + 128] = "";
+	strncpy(dir, themesDirPath, CC_PATH_SIZE_MAX);
+
+	int numOfThemes = SysListDir((const char*)dir, NULL, NULL);
+	printf("%d\n", numOfThemes);
+	if (numOfThemes <= 0) {
+		SysMakeDir(dir);
+		assets_list("data/themes/", OnAssetMgrList);
+	}
+
+	numOfThemes = SysListDir((const char*)dir, NULL, NULL);
+	if (numOfThemes <= 0) {
+		log_error("cannot extract the Themes!");
+		return NULL;
+	}
+
+	theme_arr_t* tArr = malloc(sizeof(theme_arr_t));
+	tArr->numOfEntries = numOfThemes;
+	tArr->entries = malloc(numOfThemes * sizeof(theme_t*));
+
+	for (int i = 0; i < numOfThemes; ++i) {
+		tArr->entries[i] = NULL;
+	}
+
+	SysListDir((const char*)dir, OnSysDirList, tArr);
+
+	return tArr;
+}
+
+static int OnSysDirList(const char *dir, const char *fname, void* data) {
+	theme_arr_t* tArr = (theme_arr_t*) data;
+	int pItems = tArr->numOfEntries;
+
+	for (int i = 0; i < pItems; ++i) {
+		if (tArr->entries[i] == NULL) {
+			char fullPath[2048] = "";
+			snprintf(fullPath, 2048, "%s/%s", dir, fname);
+
+			FILE* fp = fopen(fullPath, "r");
+			long int size = fsize(fp); // XX - Do Error (-1) Checking
+			char* iniTxt = (char*) malloc((size + 1) * sizeof(char));
+			memset(iniTxt, '\0', size + 1);
+			fread(iniTxt, size + 1, 1, fp);
+			tArr->entries[i] = LoadTheme(iniTxt);
+
+			free(iniTxt);
+			fclose(fp);
+			break;
+		}
+	}
+	return 0;
+}
+
+static int OnAssetMgrList(int i, const char *fname) {
+	FILE* file = NULL;
+	const char* data = NULL;
+	char* themesDirPath = SysGetThemesDir();
+	char dir[CC_PATH_SIZE_MAX + 128] = "";
+	char* fileName = SysFnameFromPath(fname);
+	snprintf(dir, CC_PATH_SIZE_MAX + 128, "%s/%s", themesDirPath, fileName);
+
+	free(fileName);
+	fileName = NULL;
+
+	data = assets_get(fname, NULL);
+	file = fopen(dir, "wb");
+	if (file) {
+		fprintf(file, "%s", data);
+		fclose(file);
+		file = NULL;
+	} else {
+		log_error("cannot open file to write");
+		return -1;
+	}
+	return 0;
 }
