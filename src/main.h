29fad15f55ab880ea2d2dcd8b8ac1df6e1599409
Aditya M <hi@pegv.in>
2022-10-27 17:31:16 +0530

    Revert "Implement Closing The File (Has Bugs, When Not Closing In A Sequential Manner The Program Crashes"
    This reverts commit 3b317f6568f7358a767ace9801714abba5a1041f.

--- src/main.h
+++ src/main.h
@@ -73,10 +73,9 @@ void SaveImageFromCanvas(std::string filepath);
 						"." + std::to_string(CS_VERSION_PATCH) + \
 						"-" + CS_BUILD_TYPE
 
-#define WINDOW_TITLE_CSTR (                                                                         \
-	CurrWS ? \
-		std::string(CurrWS->FilePath).substr(std::string(CurrWS->FilePath).find_last_of("/\\") + 1) + " - csprite " + VERSION_STR \
-		:   std::string("csprite ") + VERSION_STR \
+#define WINDOW_TITLE_CSTR (\
+		std::string(CurrWS->FilePath).substr(std::string(CurrWS->FilePath).find_last_of("/\\") + 1) \
+		+ " - csprite " + VERSION_STR                                                               \
 	).c_str()
 
 #endif // end MAIN_H
