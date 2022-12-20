262d07008aa1c1041b3407f7b98bb8d21c8934a3
pegvin <75035219+pegvin@users.noreply.github.com>
2022-12-21 00:04:25 +0530

    Add Updated Icon

--- tools/create_icons.py
+++ tools/create_icons.py
@@ -8,7 +8,7 @@ import os
 
 # Also create the application icons (in data/icons)
 if not os.path.exists('data/icons'): os.makedirs('data/icons')
-base = PIL.Image.open('data/icon-512.png').convert('RGBA')
+base = PIL.Image.open('data/icon-scaled.png').convert('RGBA')
 samplingType = None
 
 # For Compatibility Between Old & New PIL Versions
@@ -22,6 +22,6 @@ for size in [16, 24, 32, 48, 64, 128, 256, 512]:
 	img.save('data/icons/icon-%d.png' % size)
 
 img.close()
-img = PIL.Image.open('data/icon-512.png').convert('RGBA')
+img = PIL.Image.open('data/icon-scaled.png').convert('RGBA')
 img.save('data/icon.ico')
 img.close()
