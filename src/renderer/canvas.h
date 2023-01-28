77faf6a37657cfd7d13c01e0037c14e6182a2163
Aditya <75035219+pegvin@users.noreply.github.com>
2023-01-28 16:10:04 +0530

    Use SDL Renderer

--- src/renderer/canvas.h
+++ src/renderer/canvas.h
@@ -2,7 +2,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-#include <glad/glad.h>
+#include <SDL2/SDL.h>
 #include "../history.h"
 
 #ifdef __cplusplus
@@ -12,23 +12,21 @@ extern "C" {
 #define LAYER_NAME_MAX 512
 
 typedef struct {
-	char       name[LAYER_NAME_MAX];
-	GLuint     texture;
-	uint8_t*   pixels;
-	History_T* history;
+	char         name[LAYER_NAME_MAX];
+	SDL_Texture* texture;
+	uint8_t*     pixels;
+	History_T*   history;
 } CanvasLayer_T;
 
-int  Canvas_Init(int32_t w, int32_t h); // Initialize Canvas
+int  Canvas_Init(int32_t w, int32_t h, SDL_Renderer* ren); // Initialize Canvas
 void Canvas_Destroy(void); // Destroy Canvas
-void Canvas_NewFrame(bool DrawCheckerboardBg); // Call Before All Of The Calls To Canvas_Layer();
-void Canvas_Layer(CanvasLayer_T* c, bool UpdateTexture); // Specify What Layers To Render
-void Canvas_Render(GLint ViewportPosX, GLint ViewportPosY, GLsizei ViewportWidth, GLsizei ViewportHeight); // Renders The Canvas To Screen, Call After All Of The Calls To Canvas_Layer();
+void Canvas_NewFrame(bool DrawCheckerboardBg, SDL_Renderer* ren, SDL_Rect* r); // Call Before All Of The Calls To Canvas_Layer();
+void Canvas_Layer(CanvasLayer_T* c, bool UpdateTexture, SDL_Renderer* ren, SDL_Rect* r); // Specify What Layers To Render
 void Canvas_Resize(int32_t w, int32_t h); // Resize Canvas
 
 uint8_t* Canvas_GetRender();
-GLuint   Canvas_GetFBOTex();
 
-CanvasLayer_T* Canvas_CreateLayer();
+CanvasLayer_T* Canvas_CreateLayer(SDL_Renderer* ren);
 void           Canvas_DestroyLayer(CanvasLayer_T* c);
 
 #ifdef __cplusplus
