2225fe16777ebed54322ef3540fe420b2763afae
Aditya Mishra <75035219+pegvin@users.noreply.github.com>
2023-01-29 02:54:59 +0530

    Fix Canvas Preview & Make Canvas Border Adapt To Theme

--- src/renderer/canvas.c
+++ src/renderer/canvas.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include "./canvas.h"
 #include "../logger.h"
 #include "../assets.h"
@@ -5,37 +6,61 @@
 // Static For No Name Collisions
 static int32_t CanvasDims[2] = { 0, 0 };
 CanvasLayer_T bgLayer = { "", NULL, NULL, NULL };
+SDL_Texture* CanvasTex = NULL;
 
-int Canvas_Init(int32_t w, int32_t h, SDL_Renderer* ren) {
-	CanvasDims[0] = w;
-	CanvasDims[1] = h;
+SDL_Texture* Canvas_GetTex() {
+	return CanvasTex;
+}
 
-	bgLayer.pixels = (uint8_t*) malloc(w/2 * h/2 * 4 * sizeof(uint8_t));
-	for (int32_t y = 0; y < h/2; y++) {
-		for (int32_t x = 0; x < w/2; x++) {
+static void _FreeCanvasBgLayer() {
+	if (bgLayer.texture) SDL_DestroyTexture(bgLayer.texture);
+	if (bgLayer.pixels) free(bgLayer.pixels);
+	bgLayer.pixels = NULL;
+	bgLayer.texture = NULL;
+}
+
+static int _CanvasGenBgLayer(SDL_Renderer* ren) {
+	_FreeCanvasBgLayer();
+	bgLayer.pixels = (uint8_t*) malloc(CanvasDims[0]/2 * CanvasDims[1]/2 * 4 * sizeof(uint8_t));
+	for (int32_t y = 0; y < CanvasDims[1]/2; y++) {
+		for (int32_t x = 0; x < CanvasDims[0]/2; x++) {
 			uint8_t r = 0xC0, g = 0xC0, b = 0xC0, a = 0xFF;
 			if ((x + y) % 2) { r = 0x80; g = 0x80; b = 0x80; a = 0xFF; }
-			uint8_t* pixel = &bgLayer.pixels[(y * (int)(w/2) + x) * 4];
+			uint8_t* pixel = &bgLayer.pixels[(y * (int)(CanvasDims[0]/2) + x) * 4];
 			*(pixel + 0) = r;
 			*(pixel + 1) = g;
 			*(pixel + 2) = b;
 			*(pixel + 3) = a;
 		}
 	}
-	bgLayer.texture = SDL_CreateTexture(ren, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STATIC, w/2, h/2);
+	bgLayer.texture = SDL_CreateTexture(ren, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STATIC, CanvasDims[0]/2, CanvasDims[1]/2);
 	if (bgLayer.texture == NULL) {
 		Logger_Error("Cannot create bgLayer, SDL_CreateTexture() returned NULL: %s", SDL_GetError());
-		free(bgLayer.pixels);
-		bgLayer.pixels = NULL;
+		_FreeCanvasBgLayer();
 		return EXIT_FAILURE;
 	}
-	SDL_UpdateTexture(bgLayer.texture, NULL, bgLayer.pixels, w/2 * sizeof(uint8_t) * 4);
+	SDL_UpdateTexture(bgLayer.texture, NULL, bgLayer.pixels, CanvasDims[0]/2 * sizeof(uint8_t) * 4);
 
 	if (SDL_SetTextureBlendMode(bgLayer.texture, SDL_BLENDMODE_BLEND) != 0) {
 		Logger_Error("SDL_SetTextureBlendMode() returned Non-Zero: %s", SDL_GetError());
-		SDL_DestroyTexture(bgLayer.texture);
-		free(bgLayer.pixels);
-		bgLayer.pixels = NULL;
+		_FreeCanvasBgLayer();
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
+
+int Canvas_Init(int32_t w, int32_t h, SDL_Renderer* ren) {
+	CanvasDims[0] = w;
+	CanvasDims[1] = h;
+
+	if (_CanvasGenBgLayer(ren) != EXIT_SUCCESS) {
+		_FreeCanvasBgLayer();
+	}
+
+	CanvasTex = SDL_CreateTexture(ren, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_TARGET, w, h);
+	if (bgLayer.texture == NULL) {
+		Logger_Error("Cannot create CanvasTex, SDL_CreateTexture() returned NULL: %s", SDL_GetError());
+		_FreeCanvasBgLayer();
 		return EXIT_FAILURE;
 	}
 
@@ -48,28 +73,32 @@ void Canvas_Destroy(void) {
 	CanvasDims[0] = CanvasDims[1] = 0;
 }
 
-void Canvas_NewFrame(bool DrawCheckerboardBg, SDL_Renderer* ren, SDL_Rect* r) {
+void Canvas_NewFrame(bool DrawCheckerboardBg, SDL_Renderer* ren) {
+	SDL_SetRenderTarget(ren, CanvasTex);
+	SDL_RenderClear(ren);
+
 	if (DrawCheckerboardBg == true) {
-		SDL_RenderCopy(ren, bgLayer.texture, NULL, r);
+		SDL_RenderCopy(ren, bgLayer.texture, NULL, NULL);
 	}
 }
 
-void Canvas_Resize(int32_t w, int32_t h) {
+void Canvas_Resize(int32_t w, int32_t h, SDL_Renderer* ren) {
 	CanvasDims[0] = w;
 	CanvasDims[1] = h;
+
+	assert(_CanvasGenBgLayer(ren) == EXIT_SUCCESS);
 }
 
-void Canvas_Layer(CanvasLayer_T* c, bool UpdateTexture, SDL_Renderer* ren, SDL_Rect* r) {
+void Canvas_Layer(CanvasLayer_T* c, bool UpdateTexture, SDL_Renderer* ren) {
 	if (UpdateTexture == true) {
 		SDL_UpdateTexture(c->texture, NULL, c->pixels, CanvasDims[0] * sizeof(uint8_t) * 4); // Upload Pixels To Currently Selected Texture On GPU
 	}
-	SDL_RenderCopy(ren, c->texture, NULL, r);
+	SDL_RenderCopy(ren, c->texture, NULL, NULL);
 }
 
-uint8_t* Canvas_GetRender() {
-	uint8_t* data = (uint8_t*) malloc(CanvasDims[0] * CanvasDims[1] * 4 * sizeof(uint8_t));
-	if (data == NULL) return NULL;
-	return data;
+void Canvas_FrameEnd(SDL_Renderer* ren, SDL_Rect* r) {
+	SDL_SetRenderTarget(ren, NULL);
+	SDL_RenderCopy(ren, CanvasTex, NULL, r);
 }
 
 CanvasLayer_T* Canvas_CreateLayer(SDL_Renderer* ren) {
--- src/renderer/canvas.h
+++ src/renderer/canvas.h
@@ -20,12 +20,12 @@ typedef struct {
 
 int  Canvas_Init(int32_t w, int32_t h, SDL_Renderer* ren); // Initialize Canvas
 void Canvas_Destroy(void); // Destroy Canvas
-void Canvas_NewFrame(bool DrawCheckerboardBg, SDL_Renderer* ren, SDL_Rect* r); // Call Before All Of The Calls To Canvas_Layer();
-void Canvas_Layer(CanvasLayer_T* c, bool UpdateTexture, SDL_Renderer* ren, SDL_Rect* r); // Specify What Layers To Render
-void Canvas_Resize(int32_t w, int32_t h); // Resize Canvas
+void Canvas_NewFrame(bool DrawCheckerboardBg, SDL_Renderer* ren); // Call Before All Of The Calls To Canvas_Layer();
+void Canvas_Layer(CanvasLayer_T* c, bool UpdateTexture, SDL_Renderer* ren); // Specify What Layers To Render
+void Canvas_FrameEnd(SDL_Renderer* ren, SDL_Rect* r); // Must be called after Canvas_NewFrame & Canvas_Layer calls are done.
+void Canvas_Resize(int32_t w, int32_t h, SDL_Renderer* ren); // Resize Canvas
 
-uint8_t* Canvas_GetRender();
-
+SDL_Texture*   Canvas_GetTex();
 CanvasLayer_T* Canvas_CreateLayer(SDL_Renderer* ren);
 void           Canvas_DestroyLayer(CanvasLayer_T* c);
 
