abbea74959ed0c8503a56c3ef169c367e2d839b5
Aditya M <hi@pegv.in>
2023-01-31 15:43:46 +0530

    Enable Custom FPS Cap By Default

--- src/config.c
+++ src/config.c
@@ -37,23 +37,20 @@ Config_T* LoadConfig(void) {
 	if (access(configPath, F_OK) == 0) {
 		ini_t *config = ini_load(configPath);
 		const char* vsync = ini_get(config, "csprite", "vsync");
-		if (vsync == NULL)
-			s->vsync = true;
-		else if (strncmp(vsync, "true", 5) == 0)
-			s->vsync = true;
-		else
-			s->vsync = false;
+		if (vsync == NULL) s->vsync = false;
+		else if (strncmp(vsync, "true", 5) == 0) s->vsync = true;
+		else s->vsync = false;
 
 		const char* FramesUpdateRate_Str = ini_get(config, "csprite", "FramesUpdateRate");
-		s->FramesUpdateRate = FramesUpdateRate_Str == NULL ? 30 : atoi(FramesUpdateRate_Str);
+		s->FramesUpdateRate = FramesUpdateRate_Str == NULL ? 60 : atoi(FramesUpdateRate_Str);
 		s->FramesUpdateRate = s->FramesUpdateRate < 5 ? 5 : s->FramesUpdateRate;
 
 		ini_free(config);
 		config = NULL;
 		return s;
 	} else {
-		s->vsync = true;
-		s->FramesUpdateRate = 50;
+		s->vsync = false;
+		s->FramesUpdateRate = 60;
 		WriteConfig(s);
 		return s;
 	}
