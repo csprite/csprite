be93a8cd9e18dc450991a4ce356a8423240aa98e
Aditya M <hi@pegv.in>
2023-02-03 18:27:05 +0530

    Copy Opened Image Data Into Undo Buffer

--- src/main.cpp
+++ src/main.cpp
@@ -1150,8 +1150,7 @@ static void OpenNewFile() {
 			SelectedLayerIndex = 0;
 			CURR_CANVAS_LAYER = Canvas_CreateLayer(R_GetRenderer());
 			memcpy(CURR_CANVAS_LAYER->pixels, _data, w * h * 4 * sizeof(uint8_t));
-			FreeHistory(&CURR_CANVAS_LAYER->history);
-			SaveHistory(&CURR_CANVAS_LAYER->history, w * h * 4 * sizeof(uint8_t), CURR_CANVAS_LAYER->pixels);
+			memcpy(CURR_CANVAS_LAYER->history->pixels, _data, w * h * 4 * sizeof(uint8_t));
 
 			snprintf(FilePath, SYS_PATHNAME_MAX, "%s", _fName);
 			char* filePathBasename = Sys_GetBasename(_fName);
