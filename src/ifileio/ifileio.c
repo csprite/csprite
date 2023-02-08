ac6b1e08c8bfb62e96adf54b804103d46097c0cc
Aditya Mishra <75035219+pegvin@users.noreply.github.com>
2023-02-09 00:43:04 +0530

    Move Blending To File Writer

--- src/ifileio/ifileio.c
+++ src/ifileio/ifileio.c
@@ -5,13 +5,69 @@
 #include "stb_image.h"
 #include "stb_image_write.h"
 
-int ifio_write(const char* filePath, uint8_t* pixels, int32_t w, int32_t h) {
-	if (filePath == NULL || pixels == NULL || w < 1 || h < 1) return -1;
+static uint8_t clamp_u16_to_u8(uint16_t val) {
+	if (val > -1 && val < 256) return val;
+	else if (val > 255) return 255;
 
+	return 0;
+}
+
+static uint8_t* BlendPixels_Alpha(int32_t w, int32_t h, int32_t numOfLayers, CanvasLayer_T** layers) {
+	uint8_t* canvas_data = (uint8_t*) malloc(w * h * 4 * sizeof(uint8_t));
+	memset(canvas_data, 0, w * h * 4 * sizeof(uint8_t));
+
+	for (uint32_t i = 0; i < numOfLayers; ++i) {
+		if (layers[i] != NULL) {
+			for (int32_t y = 0; y < h; ++y) {
+				for (int32_t x = 0; x < w; ++x) {
+					// Simple Alpha-Blending Being Done Here.
+					uint8_t* srcPixel = GetCharData(layers[i]->pixels, x, y, w, h);
+					uint8_t* destPixel = GetCharData(canvas_data, x, y, w, h);
+					if (srcPixel != NULL && destPixel != NULL) {
+						uint8_t src1Red = *(srcPixel + 0), src1Green = *(srcPixel + 1), src1Blue = *(srcPixel + 2), src1Alpha = *(srcPixel + 3);
+						uint8_t src2Red = *(destPixel + 0), src2Green = *(destPixel + 1), src2Blue = *(destPixel + 2), src2Alpha = *(destPixel + 3);
+
+						uint16_t outRed = ((uint16_t)src1Red * src1Alpha + (uint16_t)src2Red * (255 - src1Alpha) / 255 * src2Alpha) / 255;
+						uint16_t outGreen = ((uint16_t)src1Green * src1Alpha + (uint16_t)src2Green * (255 - src1Alpha) / 255 * src2Alpha) / 255;
+						uint16_t outBlue = ((uint16_t)src1Blue * src1Alpha + (uint16_t)src2Blue * (255 - src1Alpha) / 255 * src2Alpha) / 255;
+						uint16_t outAlpha = src1Alpha + (uint16_t)src2Alpha * (255 - src1Alpha) / 255;
+
+						*(destPixel + 0) = clamp_u16_to_u8(outRed);
+						*(destPixel + 1) = clamp_u16_to_u8(outGreen);
+						*(destPixel + 2) = clamp_u16_to_u8(outBlue);
+						*(destPixel + 3) = clamp_u16_to_u8(outAlpha);
+					}
+				}
+			}
+		}
+	}
+	return canvas_data;
+}
+
+int ifio_write(const char* filePath, int32_t w, int32_t h, int32_t numOfLayers, CanvasLayer_T** layers) {
+	if (filePath == NULL || layers == NULL || numOfLayers < 1 || w < 1 || h < 1) return -1;
+
+	uint8_t* _BlendedPixels = NULL;
 	if (HAS_SUFFIX_CI(filePath, ".png", 4)) {
-		stbi_write_png(filePath, w, h, 4, pixels, 0);
+		_BlendedPixels = BlendPixels_Alpha(w, h, numOfLayers, layers);
+		if (_BlendedPixels == NULL) {
+			Logger_Error("Alpha Blending Failed, BlendPixels_Alpha(...) returned NULL");
+			return -1;
+		} else {
+			stbi_write_png(filePath, w, h, 4, _BlendedPixels, 0);
+			free(_BlendedPixels);
+			_BlendedPixels = NULL;
+		}
 	} else if (HAS_SUFFIX_CI(filePath, ".jpeg", 5) || HAS_SUFFIX_CI(filePath, ".jpg", 4)) {
-		stbi_write_jpg(filePath, w, h, 4, pixels, 100);
+		_BlendedPixels = BlendPixels_Alpha(w, h, numOfLayers, layers);
+		if (_BlendedPixels == NULL) {
+			Logger_Error("Alpha Blending Failed, BlendPixels_Alpha(...) returned NULL");
+			return -1;
+		} else {
+			stbi_write_jpg(filePath, w, h, 4, _BlendedPixels, 100);
+			free(_BlendedPixels);
+			_BlendedPixels = NULL;
+		}
 	} else {
 		Logger_Error("Error Un-supported file format: %s\n", filePath);
 		return -1;
