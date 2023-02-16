7a0a80adcdac64b04d3f1cb39e8b4a41e0f85009
Aditya M <hi@pegv.in>
2023-02-16 17:16:04 +0530

    Implement Endianess Specific Parsing & Writing

---
+++ src/ifileio/endian.c
@@ -0,0 +1,20 @@
+#include "endian.h"
+
+uint16_t swap_uint16(uint16_t val) {
+	return (val << 8) | (val >> 8);
+}
+
+int16_t swap_int16(int16_t val) {
+	return (val << 8) | ((val >> 8) & 0xFF);
+}
+
+uint32_t swap_uint32(uint32_t val) {
+	val = ((val << 8) & 0xFF00FF00 ) | ((val >> 8) & 0xFF00FF ); 
+	return (val << 16) | (val >> 16);
+}
+
+int32_t swap_int32(int32_t val) {
+	val = ((val << 8) & 0xFF00FF00) | ((val >> 8) & 0xFF00FF ); 
+	return (val << 16) | ((val >> 16) & 0xFFFF);
+}
+
