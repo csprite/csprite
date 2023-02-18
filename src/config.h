9a99f51f82b5c16e701738bd20bf4f56302e28d4
Aditya M <hi@pegv.in>
2023-02-18 16:16:59 +0530

    Add Header Guards

--- src/config.h
+++ src/config.h
@@ -1,3 +1,6 @@
+#ifndef _APP_CONFIG_H
+#define _APP_CONFIG_H 1
+
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -18,3 +21,7 @@ int WriteConfig(Config_T* s);
 #ifdef __cplusplus
 }
 #endif
+
+#endif // _APP_CONFIG_H
+
+
