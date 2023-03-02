91a83afa8b44a3a99a6b64d6b2df228940e8ace4
Aditya M <git@pegv.in>
2023-03-03 01:57:06 +0530

    Port To std::vector & std::string

--- src/ifileio/ifileio.h
+++ src/ifileio/ifileio.h
@@ -4,17 +4,9 @@
 #include <stdint.h>
 #include "../renderer/canvas.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 // Full Support For Reading & Writing PNGs & JPEGs/JPGs
-int32_t ifio_write(const char* filePath, int32_t w, int32_t h, CanvasLayerArr_T* arr);
-int32_t ifio_read(const char* filePath, int32_t* w_ptr, int32_t* h_ptr, CanvasLayerArr_T** arr);
-
-#ifdef __cplusplus
-}
-#endif
+int32_t ifio_write(const char* filePath, CanvasLayer_Manager* mgr);
+int32_t ifio_read(const char* filePath, CanvasLayer_Manager** mgr);
 
 #endif // I_FILE_IO_H_
 
