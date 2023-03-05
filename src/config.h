7f54d6ae5fa82f54f6f5a597b90d0c387b4fb977
Aditya <75035219+pegvin@users.noreply.github.com>
2023-03-05 20:55:26 +0530

    Initial commit

--- src/config.h
+++ src/config.h
@@ -1,26 +1,30 @@
 #ifndef _APP_CONFIG_H
 #define _APP_CONFIG_H 1
 
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
+#include <string>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+struct Config_T {
+	uint16_t    Max_FPS;
+	std::string Theme_Name;
+	uint8_t     CheckerboardColor1[3];
+	uint8_t     CheckerboardColor2[3];
 
-typedef struct {
-	bool    vsync;
-	int32_t FramesUpdateRate;
-} Config_T;
+	Config_T& operator = (const Config_T& a) {
+		this->Max_FPS = a.Max_FPS;
+		this->Theme_Name = a.Theme_Name;
+		this->CheckerboardColor1[0] = a.CheckerboardColor1[0];
+		this->CheckerboardColor1[1] = a.CheckerboardColor1[1];
+		this->CheckerboardColor1[2] = a.CheckerboardColor1[2];
+
+		this->CheckerboardColor2[0] = a.CheckerboardColor2[0];
+		this->CheckerboardColor2[1] = a.CheckerboardColor2[1];
+		this->CheckerboardColor2[2] = a.CheckerboardColor2[2];
+		return *this;
+	}
+};
 
 Config_T* LoadConfig(void);
 int WriteConfig(Config_T* s);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // _APP_CONFIG_H
 
