0474e66a2bc6d240bcce2fc35e8e4ad547dee895
Aditya M <hi@pegv.in>
2023-03-07 21:08:42 +0530

    Remove Extern For C

--- src/renderer/renderer.h
+++ src/renderer/renderer.h
@@ -2,18 +2,10 @@
 
 #include <SDL2/SDL.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 int R_Init(SDL_Window* window); // Initializes SDL_Renderer
 void R_Destroy(void); // Destroy ImGui & OpenGL Contexts
 void R_Clear(void); // Clear The Screen
 void R_NewFrame(void); // Call Before Calling Any ImGui Functions For Rendering The Gui
 void R_Present(void); // Render Everything & Swap Buffers
 SDL_Renderer* R_GetRenderer();
-
-#ifdef __cplusplus
-}
-#endif
 
