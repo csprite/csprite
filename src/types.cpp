1933594e72b3e93a6776f7f3ffb5258faee53b2a
Aditya M <git@pegv.in>
2023-05-13 17:16:15 +0530

    Include Implementation in Header

--- src/types.cpp
+++ src/types.cpp
@@ -19,11 +19,3 @@ inline bool Rect::operator != (const Rect& rhs) const {
 inline Rect::operator SDL_Rect() const {
 	return { x, y, w, h };
 }
-
-template <typename TypeToClamp, typename TypeToClampTo>
-TypeToClampTo clampNum(const TypeToClamp a, const TypeToClampTo targetType) {
-	auto min = std::numeric_limits<TypeToClampTo>().min();
-	auto max = std::numeric_limits<TypeToClampTo>().max();
-
-	return (a <= min ? min : a) >= max ? max : (a <= min ? min : a);
-}
