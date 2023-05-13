932675ecaac088593184000620cdb07b9a8b7e35
Aditya M <hi@pegv.in>
2023-05-13 14:06:23 +0530

    Improve Swapping Macros

--- src/ifileio/ifileio_endian.h
+++ src/ifileio/ifileio_endian.h
@@ -3,12 +3,6 @@
 
 #include <stdint.h>
 
-#if TARGET_IS_BIGENDIAN == 1
-	#define IS_BIG_ENDIAN 1
-#else
-	#define IS_BIG_ENDIAN 0
-#endif
-
 // swap_xxxxxx functions simple swap bytes, can be usefully for converting between endianness
 int16_t  swap_int16(int16_t val);
 uint16_t swap_uint16(uint16_t val);
@@ -16,11 +10,17 @@ uint16_t swap_uint16(uint16_t val);
 int32_t  swap_int32(int32_t val);
 uint32_t swap_uint32(uint32_t val);
 
-// only swap if on big-endian system
-#define SWAP_ONLY_BIGE_i32(val) (IS_BIG_ENDIAN == 1 ? swap_int32(val) : val)
-#define SWAP_ONLY_BIGE_u32(val) (IS_BIG_ENDIAN == 1 ? swap_uint32(val) : val)
-#define SWAP_ONLY_BIGE_i16(val) (IS_BIG_ENDIAN == 1 ? swap_int16(val) : val)
-#define SWAP_ONLY_BIGE_u16(val) (IS_BIG_ENDIAN == 1 ? swap_uint16(val) : val)
+#if TARGET_IS_BIGENDIAN == 1
+	#define SWAP_ONLY_BIGE_i32(val) swap_int32(val)
+	#define SWAP_ONLY_BIGE_u32(val) swap_uint32(val)
+	#define SWAP_ONLY_BIGE_i16(val) swap_int16(val)
+	#define SWAP_ONLY_BIGE_u16(val) swap_uint16(val)
+#else
+	#define SWAP_ONLY_BIGE_i32(val) val
+	#define SWAP_ONLY_BIGE_u32(val) val
+	#define SWAP_ONLY_BIGE_i16(val) val
+	#define SWAP_ONLY_BIGE_u16(val) val
+#endif
 
 #endif // _IFILEIO_ENDIAN_H
 
