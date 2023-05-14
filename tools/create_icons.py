c67a4cb4d18b24e19c512237ded4ed064c0d55f0
Aditya M <hi@pegv.in>
2023-05-14 17:25:09 +0530

    Add Icons Created By "Birds Probably"

--- tools/create_icons.py
+++ tools/create_icons.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 
 # Requires Python v3 Interpreter with pillow module installed
-
 import itertools
 import PIL.Image
 import os
 
 # Also create the application icons (in data/icons)
 if not os.path.exists('data/icons'): os.makedirs('data/icons')
-base = PIL.Image.open('data/icon-scaled.png').convert('RGBA')
+base = PIL.Image.open('data/icon-32x32.png').convert('RGBA')
 samplingType = None
 
 # For Compatibility Between Old & New PIL Versions
@@ -22,6 +21,6 @@ for size in [16, 24, 32, 48, 64, 128, 256, 512]:
 	img.save('data/icons/icon-%d.png' % size)
 
 img.close()
-img = PIL.Image.open('data/icon-scaled.png').convert('RGBA')
+img = PIL.Image.open('data/icon-32x32.png').convert('RGBA')
 img.save('data/icon.ico')
 img.close()
