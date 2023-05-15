12bc5aa02f144c60b5341b4afe0a044960aed3b5
Aditya M <hi@pegv.in>
2023-05-15 22:51:37 +0530

    Introduce A Canvas Renderer

---
+++ src/types.cpp
@@ -0,0 +1,17 @@
+#include "types.hpp"
+
+inline Rect& Rect::operator = (const Rect& rhs) {
+	x = rhs.x;
+	y = rhs.y;
+	w = rhs.w;
+	h = rhs.h;
+	return *this;
+}
+
+inline bool Rect::operator == (const Rect& rhs) const {
+	return (x == rhs.x && y == rhs.y && w == rhs.w && h == rhs.h);
+}
+
+inline bool Rect::operator != (const Rect& rhs) const {
+	return !(*this != rhs);
+}
