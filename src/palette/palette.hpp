26aad67d3c70fa3cd08e9ae831c0f474156a2e1e
Aditya M <hi@pegv.in>
2023-05-16 00:09:20 +0530

    Create A Palette Struct

---
+++ src/palette/palette.hpp
@@ -0,0 +1,19 @@
+#ifndef CSP_PALETTE_PALETTE_HPP_INCLUDED_
+#define CSP_PALETTE_PALETTE_HPP_INCLUDED_ 1
+#pragma once
+
+#include "types.hpp"
+#include "pixel/pixel.hpp"
+
+struct Palette {
+	Vector<Pixel> Colors;
+	Pixel PrimaryColor;
+
+	Pixel& operator[](u32 i);
+
+	void Add(Pixel color); // only adds if the color isn't in the palette
+	bool Contains(Pixel& color) const; // returns true if the color is in the palette
+	void Remove(Pixel& color); // removes the color in the palette (duplicates are removed too)
+};
+
+#endif // CSP_PALETTE_PALETTE_HPP_INCLUDED_
