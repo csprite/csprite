54a81669e8ecb150086f38f9226a3256337fe337
Aditya M <hi@pegv.in>
2023-05-26 14:10:18 +0530

    Organize & Structure Third Party Files

--- src/impl.c
+++
@@ -1,8 +0,0 @@
-// all header only libraries are compiled in this translation unit to reduce re-compiling...
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb/stb_image_write.h"
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb/stb_image.h"
-
