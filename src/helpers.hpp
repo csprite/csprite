fe47591f593f9525c355c512ce383ca9eb6a0a4e
Aditya <75035219+pegvin@users.noreply.github.com>
2023-05-28 00:47:48 +0530

    Generate ImGui Code Using XML Files & Python

--- src/helpers.hpp
+++ src/helpers.hpp
@@ -1,29 +1,10 @@
-#ifndef CSP_HELPERS_HPP_INCLUDE_
-#define CSP_HELPERS_HPP_INCLUDE_
+#ifndef CSP_HELPERS_HPP_INCLUDED_
+#define CSP_HELPERS_HPP_INCLUDED_
 #pragma once
 
-#include <cstdio>
-#include <cstdlib>
-#include <climits>
 #include <string>
 
-#if defined(TARGET_WINDOWS)
-	#include <windows.h>
-	#include <shellapi.h>
-#endif
+void logGLFWErrors(int error, const char* description);
+void openUrl(std::string url);
 
-void logGLFWErrors(int error, const char *description) {
-	printf("Error: %d\n%s\n", error, description);
-}
-
-void openUrl(std::string url) {
-#if defined(__linux__) || defined(__FreeBSD__)
-	system(("xdg-open \"" + url + "\"").c_str());
-#elif defined(__APPLE__)
-	system(("open \"" + url + "\"").c_str());
-#elif defined(_WIN32)
-	ShellExecute(0, 0, url.c_str(), 0, 0, SW_SHOW);
-#endif
-}
-
-#endif // CSP_HELPERS_HPP_INCLUDE_
+#endif // CSP_HELPERS_HPP_INCLUDED_
