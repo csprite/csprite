e45175c019ea17d0f254bbdee900296c43843ade
Aditya M <git@pegv.in>
2023-05-31 01:55:52 +0530

    Remove Unused Header Files

--- src/renderer/canvas.hpp
+++ src/renderer/canvas.hpp
@@ -3,18 +3,11 @@
 #pragma once
 
 #include "types.hpp"
-#include "imgui/imgui.h"
-#include "glad/glad.h"
 #include "pixel/pixel.hpp"
 
-/* Functions For Canvas Rendering.
-    - The canvas is basically the final rendered image that is shown to the screen,
-	  
-*/
-
 struct Canvas {
-	using TextureID = GLuint;
-	TextureID id = 0;
+	using GLuint = unsigned int;
+	GLuint id = 0;
 	RectF32 viewport = { 0, 0, 0, 0 };
 
 	Canvas(u16 w, u16 h);
