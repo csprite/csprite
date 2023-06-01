ea8538ca75c622f2c1493ce320e172ef05e2d680
Aditya M <hi@pegv.in>
2023-06-01 18:52:04 +0530

    Simplify The Code More

--- src/main.h
+++ src/main.h
@@ -1,24 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <string>
-#include "types.hpp"
-#include "pixel/pixel.hpp"
-
-enum export_format_e { PNG, JPG };
-
 // Canvas Zoom
 void ZoomNCenterVP(); // Zooms And Centers The Viewport.
 void AdjustZoom(bool increase);
-
-// File IO
-std::string FixFileExtension(std::string filepath);
-void SaveImageFromCanvas(std::string filepath);
-
-// Undo/Redo Related
-int Undo();
-int Redo();
-void SaveState();
-void FreeHistory();
 
 #endif // end MAIN_H
