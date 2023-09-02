2d477f8f68fdf9f4530de1ec6589e363141ff057
pegvin <75035219+pegvin@users.noreply.github.com>
2023-09-02 14:41:20 +0530

    Fix != Operator Recursion

--- src/types.cpp
+++ src/types.cpp
@@ -12,7 +12,7 @@
 		return (x == rhs.x && y == rhs.y && w == rhs.w && h == rhs.h); \
 	} \
 	bool name::operator != (const name& rhs) const { \
-		return !(*this != rhs); \
+		return !(*this == rhs); \
 	}
 
 GEN_STRUCT_OVERLOAD_FUNCS(RectI32)
