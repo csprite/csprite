78abec84310e5e141a937288796a856bee8e2a9b
pegvin <75035219+pegvin@users.noreply.github.com>
2023-09-02 17:57:07 +0530

    Refactor Code

--- tools/create_icons.py
+++ tools/create_icons.py
@@ -3,18 +3,18 @@
 # Requires Python v3 Interpreter with pillow module installed
 
 import itertools
-import PIL.Image
+from PIL import Image
 import os
 
 # Also create the application icons (in data/icons)
 if not os.path.exists('data/icons'): os.makedirs('data/icons')
-base = PIL.Image.open('data/icon-32x32.png').convert('RGBA')
+base = Image.open('data/icon-32x32.png').convert('RGBA')
 
 for size in [16, 24, 32, 48, 64, 128, 256, 512]:
-	img = base.resize((size, size), PIL.Image.Resampling.NEAREST)
+	img = base.resize((size, size), Image.Resampling.NEAREST)
 	img.save('data/icons/icon-%d.png' % size)
 
 img.close()
-img = PIL.Image.open('data/icon-32x32.png').convert('RGBA')
+img = Image.open('data/icon-32x32.png').convert('RGBA')
 img.save('data/icon.ico')
 img.close()
