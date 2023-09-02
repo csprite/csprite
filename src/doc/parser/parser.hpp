05ab9ff47928cf692c7b0a3b3a5248bff9ff7dc8
pegvin <75035219+pegvin@users.noreply.github.com>
2023-09-02 21:36:58 +0530

    Link With libpng

---
+++ src/doc/parser/parser.hpp
@@ -0,0 +1,12 @@
+#ifndef CSP_DOC_PARSER_PARSER_HPP_INCLUDED_
+#define CSP_DOC_PARSER_PARSER_HPP_INCLUDED_
+#pragma once
+
+#include "doc/doc.hpp"
+
+namespace Parser {
+	Doc* ParseImageFile(const char* const filePath);
+}
+
+#endif // CSP_DOC_PARSER_PARSER_HPP_INCLUDED_
+
