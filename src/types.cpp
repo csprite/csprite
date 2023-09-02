21e309a6963668cd8ebb9ea5669894db4d941b41
pegvin <75035219+pegvin@users.noreply.github.com>
2023-09-02 14:40:52 +0530

    Remove Unused Macro Params & Remove inline keyword causing linking issues

--- src/types.cpp
+++ src/types.cpp
@@ -1,19 +1,20 @@
 #include "types.hpp"
 
-#define GEN_STRUCT_OVERLOAD_FUNCS(name, type) \
-	inline name& name::operator = (const name& rhs) { \
+#define GEN_STRUCT_OVERLOAD_FUNCS(name) \
+	name& name::operator = (const name& rhs) { \
 		x = rhs.x; \
 		y = rhs.y; \
 		w = rhs.w; \
 		h = rhs.h; \
 		return *this; \
 	} \
-	inline bool name::operator == (const name& rhs) const { \
+	bool name::operator == (const name& rhs) const { \
 		return (x == rhs.x && y == rhs.y && w == rhs.w && h == rhs.h); \
 	} \
-	inline bool name::operator != (const name& rhs) const { \
+	bool name::operator != (const name& rhs) const { \
 		return !(*this != rhs); \
 	}
 
-GEN_STRUCT_OVERLOAD_FUNCS(RectI32, i32)
-GEN_STRUCT_OVERLOAD_FUNCS(RectF32, f32)
+GEN_STRUCT_OVERLOAD_FUNCS(RectI32)
+GEN_STRUCT_OVERLOAD_FUNCS(RectF32)
+
