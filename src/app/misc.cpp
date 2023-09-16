242093a51367e4ffcd8d817e5bffd67f0b54adee
pegvin <75035219+pegvin@users.noreply.github.com>
2023-09-16 21:22:43 +0530

    Use System Types

--- src/app/misc.cpp
+++ src/app/misc.cpp
@@ -4,14 +4,14 @@
 
 void EnableVT100() {
 #ifdef TARGET_WINDOWS
-	u32 iMode = 0;
-	int iHandle = GetStdHandle(STD_INPUT_HANDLE);
+	DWORD iMode = 0;
+	HANDLE iHandle = GetStdHandle(STD_INPUT_HANDLE);
 	if (iHandle == INVALID_HANDLE_VALUE || iHandle == NULL) return;
 	GetConsoleMode(iHandle, &iMode);
 	SetConsoleMode(iHandle, iMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
 
-	u32 oMode = 0;
-	int oHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	DWORD oMode = 0;
+	HANDLE oHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 	if (oHandle == INVALID_HANDLE_VALUE || oHandle == NULL) return;
 	GetConsoleMode(oHandle, &oMode);
 	SetConsoleMode(oHandle, oMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
