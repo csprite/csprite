9651211d6b9a728650a7dc4219e2113006fe3f1c
Aditya M <git@pegv.in>
2023-09-17 21:16:34 +0530

    Add a Config System & Load Required Language Character Ranges From File

---
+++ src/app/config.cpp
@@ -0,0 +1,47 @@
+#include <cstdio>
+
+#include "app/config.hpp"
+#include "fs/fs.hpp"
+#include "log/log.h"
+
+#include "nlohmann/json.hpp"
+using json = nlohmann::json;
+
+static Cfg::Config conf;
+
+Cfg::Config& Cfg::Get() {
+	return conf;
+}
+
+void Cfg::Load() {
+	FILE* f = fopen(Fs::GetConfigFile().c_str(), "r");
+	if (f == NULL) {
+		log_error("fopen(...) - %s", strerror(errno));
+		return;
+	}
+
+	json p = json::parse(f);
+	conf.fps = p["FPS"];
+	conf.langFileName = p["LangFile"];
+
+	fclose(f);
+}
+
+void Cfg::Write() {
+	FILE* f = fopen(Fs::GetConfigFile().c_str(), "w");
+	if (f == NULL) {
+		log_error("fopen(...) - %s", strerror(errno));
+		return;
+	}
+
+	json p;
+	p["FPS"] = conf.fps;
+	p["LangFile"] = conf.langFileName;
+	auto str = p.dump(1, '\t', false);
+	if (!str.empty()) {
+		fwrite(str.data(), 1, str.size(), f);
+	}
+
+	fclose(f);
+}
+
