0ef234c7e237502b341671d81c32fada1012122c
Aditya M <hi@pegv.in>
2024-08-10 00:10:07 +0530

    Restructure Codebase

---
+++ src/types.h
@@ -0,0 +1,11 @@
+#ifndef CSPRITE_SRC_TYPES_H_INCLUDED_
+#define CSPRITE_SRC_TYPES_H_INCLUDED_
+#pragma once
+
+#include <stdint.h>
+
+typedef struct {
+	int64_t min_x, min_y, max_x, max_y;
+} mmRect_t;
+
+#endif
