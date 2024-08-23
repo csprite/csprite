c2bf133cb19fd82d410ff9a8ce2bf784b3eba686
Aditya M <hi@pegv.in>
2024-08-23 15:24:00 +0530

    Remove Old Code

--- src/tools/ui.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CSP_SRC_TOOLS_UI_HPP_INCLUDED_
-#define CSP_SRC_TOOLS_UI_HPP_INCLUDED_ 1
-#pragma once
-
-#include "tools/ToolManager.hpp"
-
-void Tools_UI_Draw(const Tool::Manager& mgr);
-
-#endif // CSP_SRC_TOOLS_UI_HPP_INCLUDED_
