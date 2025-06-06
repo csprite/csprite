183eb0ab706b3804eacb553d5de5541e7c66caf1
Aditya M <git@pegv.in>
2025-06-06 22:30:09 +0530

    Enable Padding Related Warnings

--- src/cimgui/impl.cpp
+++ src/cimgui/impl.cpp
@@ -1,10 +1,27 @@
 // Unity Build
 
+#if defined(__GNUC__)
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wpedantic"
+	#pragma GCC diagnostic ignored "-Wpadded"
+#elif defined(__clang__)
+	#pragma clang diagnostic push
+	#pragma clang diagnostic ignored "-Wpedantic"
+	#pragma GCC diagnostic ignored "-Wpadded"
+#endif
+
 #include "cimgui/imgui/imgui.cpp"
 #include "cimgui/imgui/imgui_draw.cpp"
 #include "cimgui/imgui/imgui_tables.cpp"
 #include "cimgui/imgui/imgui_widgets.cpp"
 #include "cimgui/imgui/imgui_impl_opengl3.cpp"
 #include "cimgui/imgui/imgui_impl_glfw.cpp"
+#include "cimgui/cimgui.cpp"
+
+#if defined(__GNUC__)
+	#pragma GCC diagnostic pop
+#elif defined(__clang__)
+	#pragma clang diagnostic pop
+#endif
 
 // #include "cimgui/cimgui.cpp"
