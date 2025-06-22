a286a51944cc2b71afbcc091c70f5c23c6bcb0fc
Aditya M <git@pegv.in>
2025-06-22 21:04:53 +0530

    Add Implementations

--- src/os/os.c
+++ src/os/os.c
@@ -17,43 +17,10 @@ B32 os_handle_is_zero(OS_Handle a) {
 	return a.value == 0;
 }
 
-#ifdef TARGET_WINDOWS
-	#include <windows.h>
-#endif
-
-NORETURN void os_abort(S32 exit_code) {
-#ifdef TARGET_WINDOWS
-	ExitProcess(exit_code);
-#else
-	exit(exit_code);
-#endif
-}
-
 NORETURN void os_abort_with_message(S32 exit_code, String8 message) {
 	os_show_message_box(OS_MessageBoxIcon_Error, str8_lit("Error Occurred!"), message);
 	os_abort(exit_code);
 }
-
-#ifdef TARGET_LINUX
-#include <time.h>
-
-U64 os_now_microseconds(void) {
-	struct timespec ts = {0};
-	clock_gettime(CLOCK_MONOTONIC, &ts);
-	/* Seconds & Nanoseconds Conversion To Microseconds */
-	U64 result = (ts.tv_sec * 1E+6) + (ts.tv_nsec / 1E+3);
-	return result;
-}
-
-U64 os_now_milliseconds(void) {
-	struct timespec ts = {0};
-	clock_gettime(CLOCK_MONOTONIC, &ts);
-	U64 result = ts.tv_sec * 1E+3;
-	return result;
-}
-#else
-	#error No Implementation Found!
-#endif
 
 // TODO(pegvin) - Use the respective platform's provided API
 void os_sleep_milliseconds(U32 msec) {
@@ -61,24 +28,6 @@ void os_sleep_milliseconds(U32 msec) {
 	U64 diff = 0;
 	// `diff` MIGHT be equal to `start` if os_now_milliseconds fails.
 	while ((diff = start - os_now_milliseconds()) < msec && diff != start);
-}
-
-#include <sys/mman.h>
-
-void* os_memory_reserve(U64 size) {
-	return mmap(NULL, size, PROT_NONE, MAP_PRIVATE | MAP_ANON, -1, 0);
-}
-
-void os_memory_unreserve(void* memory, U64 size) {
-	munmap(memory, size);
-}
-
-void os_memory_commit(void* memory, U64 size) {
-	mprotect(memory, size, PROT_READ | PROT_WRITE);
-}
-
-void os_memory_uncommit(void* memory, U64 size) {
-	mprotect(memory, size, PROT_NONE);
 }
 
 Rng1DU64 os_path_basename(String8 path) {
