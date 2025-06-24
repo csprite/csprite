8ccc5e7ad5ac90b4bfe7dbd11886fd8b980d1ab3
Aditya M <hi@pegv.in>
2025-06-24 20:32:44 +0530

    Comment Out Temporary Code

--- src/os/win32/gfx.c
+++ src/os/win32/gfx.c
@@ -5,24 +5,24 @@
 #include <windows.h>
 #include <shellapi.h>
 
-OS_Handle os_window_init(U64 width, U64 height, String8 title) {
-	WNDCLASSEXW wc = {
-		sizeof(wc),
-		CS_OWNDC,
-		WndProc,
-		0L,
-		0L,
-		GetModuleHandle(NULL),
-		NULL,
-		NULL,
-		NULL,
-		NULL,
-		L"csprite (Remember To Make This Dynamic)",
-		NULL
-	};
-	RegisterClassExW(&wc);
-	HWND hwnd = CreateWindowW(wc.lpszClassName, L"Dear ImGui Win32+OpenGL3 Example", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
-}
+// OS_Handle os_window_init(U64 width, U64 height, String8 title) {
+// 	WNDCLASSEXW wc = {
+// 		sizeof(wc),
+// 		CS_OWNDC,
+// 		WndProc,
+// 		0L,
+// 		0L,
+// 		GetModuleHandle(NULL),
+// 		NULL,
+// 		NULL,
+// 		NULL,
+// 		NULL,
+// 		L"csprite (Remember To Make This Dynamic)",
+// 		NULL
+// 	};
+// 	RegisterClassExW(&wc);
+// 	HWND hwnd = CreateWindowW(wc.lpszClassName, L"Dear ImGui Win32+OpenGL3 Example", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
+// }
 
 void os_open_in_browser(String8 url) {
 	ShellExecute(0, 0, (char*)url.str, 0, 0, SW_SHOW);
