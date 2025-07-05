e16ed886bc07d5cb535e2634464815c51d1c1071
Aditya M <hi@pegv.in>
2025-07-05 17:27:51 +0530

    Update src/app/imgui.h

--- src/app/imgui.h
+++ src/app/imgui.h
@@ -4,10 +4,11 @@
 
 // Wrapper For cimgui.h To disable warnings
 
-#if defined(__GNUC__)
+#if (defined(__GNUC__) || defined(__GNUG__)) && !defined(__clang__) // Because clang defines __GNUC__
 	#pragma GCC diagnostic push
 	#pragma GCC diagnostic ignored "-Wpedantic"
 	#pragma GCC diagnostic ignored "-Wpadded"
+	#pragma GCC diagnostic ignored "-Wtypedef-redefinition"
 #elif defined(__clang__)
 	#pragma clang diagnostic push
 	#pragma clang diagnostic ignored "-Wpedantic"
@@ -17,7 +18,7 @@
 
 #include "cimgui/cimgui.h"
 
-#if defined(__GNUC__)
+#if (defined(__GNUC__) || defined(__GNUG__)) && !defined(__clang__)
 	#pragma GCC diagnostic pop
 #elif defined(__clang__)
 	#pragma clang diagnostic pop
